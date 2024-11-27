bbd6036b9849f87a3c48e81e9b08cca2db85cfc3
Anil Dias Davis <anildiasdavis@gmail.com>
2024-11-27 12:34:12 +0530

    load local env file to read ENV variables
    - add an example local env file for reference

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,13 @@ module QLink
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value.to_s
+      end if File.exist?(env_file)
+    end
+
 
     config.exceptions_app = self.routes
 
